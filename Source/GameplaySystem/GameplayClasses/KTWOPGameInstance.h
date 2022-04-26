0dc9573c68dd1877dc6db0a7cded8fad7e9a59f6
Valduy <whnpir@gmail.com>
2022-04-26 22:22:30 +0300

    Add game instance

---
+++ Source/GameplaySystem/GameplayClasses/KTWOPGameInstance.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "KTWOPGameInstance.generated.h"
+
+UCLASS()
+class GAMEPLAYSYSTEM_API UKTWOPGameInstance : public UGameInstance {
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int Souls;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int Stones;
+
+	UFUNCTION(BlueprintCallable)
+	void IncreaseSouls(int amount);
+
+	UFUNCTION(BlueprintCallable)
+	void DecreaseSouls(int amount);
+
+	UFUNCTION(BlueprintCallable)
+	void IncreaseStones(int amount);
+
+	UFUNCTION(BlueprintCallable)
+	void DecreaseStones(int amount);
+};
